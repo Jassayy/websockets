25d2e9e17637d6f3d36766ff94a8e5f98aeff983
Jassayy <jasgandhi2004@gmail.com>
2024-11-13 18:38:44 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import path from "path";
+
 import dotenv from "dotenv";
 import { app, server } from "./socket/socket.js";
 
@@ -9,6 +11,8 @@ import userRoutes from "./routes/user.routes.js";
 import connectToDB from "./db/database.js";
 import cookieParser from "cookie-parser";
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); //parse incoming request with json
@@ -17,6 +21,12 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+     res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(process.env.PORT || 5000, () => {
      connectToDB();
